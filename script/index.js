479163887957cc99fe0fe4e00a9d7e70a4d8a218
Alexander <Rav89515139989@yandex.ru>
2022-03-24 18:22:08 +0300

    Feat: Add popup

--- script/index.js
+++ script/index.js
@@ -1,38 +1,52 @@
 const page = document.querySelector('.page'); //контейнер в котором лежит весь контент
 const profileButtonEdit = page.querySelector('.profile__button-edit');
-const popup = page.querySelector('.popup');
-const popupButtonClose = page.querySelector('.popup__button-close');
+const profileButtonAdd = page.querySelector('.profile__button-add');
+const popupEditInfo = page.querySelector('#popupEditInfo');
+const popupAddCard = page.querySelector('#popupAddCard');
+const popupButtonCloseEdit = page.querySelector('#buttonCloseEdit');
+const popupButtonCloseAdd = page.querySelector('#buttonCloseAdd');
 const profileName = page.querySelector('.profile__title');
 const profileJob = page.querySelector('.profile__job');
 const editProfileName = page.querySelector('#inputEditName');
 const editPtofileJob = page.querySelector('#inputEditJob');
 const formElement = page.querySelector('.popup__container')
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+function openPopupEdit() {
+  popupEditInfo.classList.add('popup_opened');
   editProfileName.value = profileName.textContent
   editPtofileJob.value = profileJob.textContent
 }
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopupEdit() {
+  popupEditInfo.classList.remove('popup_opened');
 }
+function openPopupAdd() {
+  popupAddCard.classList.add('popup_opened');
+  editProfileName.value = profileName.textContent
+  editPtofileJob.value = profileJob.textContent
+}
+function closePopupAdd() {
+  popupAddCard.classList.remove('popup_opened');
+}
+
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = editProfileName.value;
   profileJob.textContent = editPtofileJob.value;
-  closePopup()
+  closePopupEdit()
 }
 
 /*function closePopupByClickOnOverlay (event) {
   if (event.target !== event.currentTarget) {
     return;
   } else {
-    closePopup()
+    closePopupEdit()
   }
 }-----------закритие попак по облости вокруг формы*/
 
-profileButtonEdit.addEventListener('click', openPopup);
-popupButtonClose.addEventListener('click', closePopup);
+profileButtonEdit.addEventListener('click', openPopupEdit);
+profileButtonAdd.addEventListener('click', openPopupAdd);
+popupButtonCloseEdit.addEventListener('click', closePopupEdit);
+popupButtonCloseAdd.addEventListener('click', closePopupAdd);
 formElement.addEventListener('submit', formSubmitHandler);
-//popup.addEventListener('click', closePopupByClickOnOverlay)
+//popupEditInfo.addEventListener('click', closePopupByClickOnOverlay)
