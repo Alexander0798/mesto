31feacfaf66f87d2f308c9ebc3d80ad7bf676fd4
Alexander <Rav89515139989@yandex.ru>
2022-03-25 15:15:24 +0300

    feat: add card + button remove + button like

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+
 const page = document.querySelector('.page'); //контейнер в котором лежит весь контент
 const profileButtonEdit = page.querySelector('.profile__button-edit');
 const profileButtonAdd = page.querySelector('.profile__button-add');
@@ -10,6 +11,15 @@ const profileJob = page.querySelector('.profile__job');
 const editProfileName = page.querySelector('#inputEditName');
 const editPtofileJob = page.querySelector('#inputEditJob');
 const formElement = page.querySelector('.popup__container')
+
+const galleryContainer = page.querySelector('.gallery')
+const itemTemlateContent = document.querySelector('#item-temlate').content;
+const cardGalleryImg = itemTemlateContent.querySelector('.card__img');
+const cardGallerylabel = itemTemlateContent.querySelector('.card__label');
+const cardGalleryButtonRemove = itemTemlateContent.querySelector('.card__button-remove');
+const cardGalleryButtonLike = itemTemlateContent.querySelector('.card__button-like');
+const cardGalleryElement = itemTemlateContent.querySelector('.card__item')
+const cardGalleryLike = itemTemlateContent.querySelector('.card__button-like')
 
 function openPopupEdit() {
   popupEditInfo.classList.add('popup_opened');
@@ -50,3 +60,61 @@ popupButtonCloseEdit.addEventListener('click', closePopupEdit);
 popupButtonCloseAdd.addEventListener('click', closePopupAdd);
 formElement.addEventListener('submit', formSubmitHandler);
 //popupEditInfo.addEventListener('click', closePopupByClickOnOverlay)
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function setEventListners (itemElement) {
+  itemElement.querySelector('.card__button-remove').addEventListener('click', hendleDelite)
+  itemElement.querySelector('.card__button-like').addEventListener('click', function(evt) {
+    event.target.classList.toggle('card_like-active')
+  })
+}
+function hendleDelite (event) {
+  const itemElement = event.target.closest('.card__item')
+  itemElement.remove();
+}
+
+function renderItem({name, link}) {
+  const itemElement = cardGalleryElement.cloneNode(true);
+  cardGallerylabel.textContent = name;
+  cardGalleryImg.src = link;
+  cardGalleryImg.alt = `картинка ${name}`;
+  setEventListners(itemElement);
+  galleryContainer.append(itemElement);
+}
+
+const cardInfo = initialCards.map(function (item) {
+  return {
+    name: item.name,
+    link: item.link
+  };
+});
+function render() {
+  cardInfo.forEach(renderItem)
+}
+render()
