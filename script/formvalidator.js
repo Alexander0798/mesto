1934786f076532073806675692ff32839e164454
Alexander <Rav89515139989@yandex.ru>
2022-05-02 16:15:06 +0300

    refactoring

--- script/formvalidator.js
+++ script/formvalidator.js
@@ -5,17 +5,17 @@ export class FormValidator {
     this._inputInvalid = config.inputErrorClass;
     this._submit = config.submitButtonSelector;
     this._inactiveButton = config.inactiveButtonClass;
-    this._errorVisible =  config.errorVisible;
+    this._errorVisible = config.errorVisible;
     this._formElement = formElement;
     this._formInput = this._formElement.querySelectorAll(this._input)
   }
-// не валидный элемент
+  // не валидный элемент
   _hasInvalidInput() {
-   return this._inputList.some((inputElement) => {
-     return !inputElement.validity.valid
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid
     })
   }
-// проверка формы на валидновсь
+  // проверка формы на валидновсь
   _isValid(formInput) {
     if (!formInput.validity.valid) {
       this._showInputError(formInput, formInput.validationMessage);
@@ -24,7 +24,7 @@ export class FormValidator {
     }
   }
 
-// переключение кнопки сабмит
+  // переключение кнопки сабмит
   toogleButtonState() {
     if (this._hasInvalidInput()) {
       this._buttonElement.classList.add(this._inactiveButton)
@@ -34,33 +34,33 @@ export class FormValidator {
       this._buttonElement.removeAttribute('disabled')
     }
   }
-// элемент с ошибкой
-  getErrorElement(formInput){
-   return formInput.closest('.popup__label').querySelector('.popup__error');
+  // элемент с ошибкой
+  getErrorElement(formInput) {
+    return formInput.closest('.popup__label').querySelector('.popup__error');
   }
-// ошибка видна
+  // ошибка видна
   _showInputError(formInput, errorMessege) {
     const errorElement = this.getErrorElement(formInput)
     errorElement.classList.add(this._errorVisible);
     errorElement.textContent = errorMessege
     formInput.classList.add(this._inputInvalid);
   }
-// ошибка скрыта
-  _hideInputError (formInput) {
+  // ошибка скрыта
+  _hideInputError(formInput) {
     const errorElement = this.getErrorElement(formInput)
     errorElement.classList.remove(this._errorVisible);
     errorElement.textContent = '';
     formInput.classList.remove(this._inputInvalid);
   }
-// сброс полей ошибки
-  ressetValidForm(){
+  // сброс полей ошибки
+  ressetValidForm() {
     this._formInput.forEach((formElement) => {
-      this._hideInputError (formElement)
+      this._hideInputError(formElement)
     })
     this.toogleButtonState()
   }
 
-// передаёт данные для проверки на валидность
+  // передаёт данные для проверки на валидность
   enableValidation() {
     this._inputList = Array.from(this._formElement.querySelectorAll(this._input));
     this._buttonElement = this._formElement.querySelector(this._submit);
